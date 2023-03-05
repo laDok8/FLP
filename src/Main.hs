f91f3663100f5ec9e09ae3f0640b2331b040fb0d
Ladislav Dokoupil <ldokoupi@redhat.com>
2023-03-05 18:36:57 +0100

    brute force works with IO

--- src/Main.hs
+++ src/Main.hs
@@ -9,10 +9,8 @@ import Types
 import ParseInput
 import Minimize
 
-
-
-
 main = do
+    -- TODO args parse
     putStrLn "What's your first name?"
     firstName <- getLine
     let bigFirstName = map toUpper firstName
@@ -20,17 +18,8 @@ main = do
     withFile "test/test01.in" ReadMode (\handle -> do
             contents <- hGetContents handle
             putStrLn contents
+            let inputSack = getKnapsackProblem contents
+            putStrLn $ show inputSack
+            outputSack <- solveKnapSack (SackM 0 0 []) inputSack
+            print outputSack
             )
-    -- delete example
-    --its :: [(Int,Int)]
-    let its = [(10,60),(20,100),(30,120)]
-    --its2 :: [(Int,Int)]
-    let its2 = [(4,1),(5,2),(1,3)]
-    --singleton :: SackM
-    let singleton = SackM 0 0 []
-    res <- solveKnapSack singleton 42 its2
-    print res
-
-
-
-str = "Knapsack {\nmaxWeight: 46\nminCost: 324\nitems: [\n    Item {\n    weight: 36\n    cost: 3\n    }\n      Item {\n    weight: 36\n    cost: 3\n    }\n]\n}\n"
--- src/Minimize.hs
+++ src/Minimize.hs
@@ -1,6 +1,5 @@
 module Minimize
-( solveKnapSack1
-, solveKnapSack
+( solveKnapSack
 ) where
 
 --imports
@@ -10,17 +9,12 @@ import Data.Char
 import Types
 
 
---solveKnapSack1 :: SackInput -> SackM
-solveKnapSack1 :: IO SackM
-solveKnapSack1 = solveKnapSack (SackM 0 0 []) 42 [(4,1),(5,2),(1,3)]
-
-
 --brute force solution
-solveKnapSack :: SackM -> Int -> [(Int,Int)] -> IO SackM
-solveKnapSack (SackM weight cost its) _ [] = return (SackM weight cost its)
-solveKnapSack (SackM weight cost its) maxW (x:xs)
-    | (fst x) + weight > maxW = solveKnapSack (sackInsert (SackM weight cost its) (-1) (-1)) maxW xs
+solveKnapSack :: SackM -> SackInput -> IO SackM
+solveKnapSack (SackM wg ct its) (SackInput _ _ []) = return (SackM wg ct its)
+solveKnapSack (SackM wg ct its) (SackInput maxW minC (x:xs))
+    | (weight x) + wg > maxW = solveKnapSack (SackM wg ct (its ++ [0])) (SackInput maxW minC xs)
     | otherwise = do
-              s1 <- solveKnapSack (sackInsert (SackM weight cost its) (fst x) (snd x)) maxW xs
-              s2 <- solveKnapSack (sackInsert (SackM weight cost its) (-1) (-1)) maxW xs
-              return $ max s1 s2
+          s1 <- solveKnapSack (sackInsert (SackM wg ct its) (weight x) (cost x)) (SackInput maxW minC xs)
+          s2 <- solveKnapSack (SackM wg ct (its ++ [0])) (SackInput maxW minC xs)
+          return $ max s1 s2
