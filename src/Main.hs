82c6809d10c8df19e5727a8eeef15fbaa1b667c9
Ladislav Dokoupil <ldokoupi@redhat.com>
2023-03-25 15:49:39 +0100

    arg parsing

--- src/Main.hs
+++ src/Main.hs
@@ -4,22 +4,30 @@ import qualified Data.List as List
 import qualified Data.Map as Map
 import Data.Char
 import System.IO
+import System.Environment
 
 import Types
 import ParseInput
 import Minimize
 
 main = do
-    -- TODO args parse
-    putStrLn "What's your first name?"
-    firstName <- getLine
-    let bigFirstName = map toUpper firstName
-    putStrLn $ "hey " ++ bigFirstName ++ ", sup"
-    withFile "test/test01.in" ReadMode (\handle -> do
-            contents <- hGetContents handle
-            putStrLn contents
-            let inputSack = getKnapsackProblem contents
-            putStrLn $ show inputSack
-            outputSack <- return $ solveKnapSackGA inputSack
-            putStrLn $ show outputSack
-            )
+    args <- getArgs
+
+    handle <- if (args == []) || (List.isPrefixOf "-" $ last args) then return stdin else do
+        let inputFileName = last args
+        openFile inputFileName ReadMode
+    contents <- hGetContents handle
+
+    let inputSack = getKnapsackProblem contents
+    if "-i" `elem` args then do
+        putStrLn $ show inputSack
+    else if "-b" `elem` args then do
+        outputSack <- return $ solveKnapSackBrute inputSack
+        putStrLn $ if outputSack == [] then "False" else show outputSack
+    else if "-o" `elem` args then do
+        outputSack <- return $ solveKnapSackGA inputSack
+        putStrLn $ if outputSack == [] then "False" else show outputSack
+    else do
+        putStrLn "No option specified"
+
+    hClose handle
