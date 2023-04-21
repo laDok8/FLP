c301aeec7b3b0ef8e71c0359b1cc1e21214e6e2b
Ladislav Dokoupil <ldokoupi@redhat.com>
2023-04-21 17:16:34 +0200

    step works

--- proj2/src/flp22-log.pl
+++ proj2/src/flp22-log.pl
@@ -19,19 +19,45 @@ read_lines(Ls) :-
     read_line(L, C),
     (C == end_of_file, Ls=[] ;
     read_lines(LLs), Ls = [L|LLs]).
-% above taken from labs
+% ^^^ taken from labs
 
 process_lines([], _) :- fail.
 process_lines([L], L).
 process_lines([LL|LLs],Ret) :- process_line(LL), process_lines(LLs,Ret).
 
 
+:- dynamic state/4.
 process_line([S1, ' ', A, ' ', S2, ' ', B]) :-
-    %not(state(S1, A, S2, B)),
-    assert(state(S1, A, S2, B)).
+    state(S1, A, S2, B) -> true;assert(state(S1, A, S2, B)).
 
-print_list([]):-nl. %nl = newline
-print_list([H|T]):-write(H),print_list(T).
+
+
+% print list with Head
+print_word(Word, _, _, CurP) :- length(Word, Len), CurP >= Len, nl,!.
+print_word(Word, State, HeadPos, CurP) :-
+    CurP == HeadPos -> 
+        write(State), nth0(CurP, Word, Symb), write(Symb), Nex is CurP + 1, print_word(Word, State, HeadPos, Nex);
+        (nth0(CurP, Word, Symb), write(Symb), Nex is CurP + 1, print_word(Word, State, HeadPos, Nex)).
+
+
+
+replace_nth0(Index, List, New, Result) :-
+    nth0(Index,List,Old),
+    nth0(Index, List, Old, Rest),
+    nth0(Index, Result, New, Rest).
+
+% todo check end
+% generate 1 step State, Pos, Word
+makeStep(State, Pos, Word, NState, NPos, NWord) :-
+    nth0(Pos, Word, CurSymb),
+    state(State, CurSymb, NState, NSymb),
+    (NSymb == 'R' -> 
+        NPos is Pos + 1,NWord = Word;
+        (NSymb == 'L' -> 
+            NPos is Pos - 1; 
+            (NPos is Pos,replace_nth0(Pos, Word, NSymb, NWord),!
+    ))).
+%usage: makeStep('S', 0, [a,b,c], NState, NPos, NWord).
 
 main :-
         % read stdin
@@ -40,6 +66,7 @@ main :-
         % process input -> assert States using dynamic predicate and returns Input State
         dynamic(state/4),
         process_lines(In, InpWord),
-        % prepend S symbol as Start State
-        print_list(["S"|InpWord]).
+        print_word(InpWord, 'S', 0, 0),
+        makeStep('S', 0, InpWord, NState, NPos, NWord),
+        print_word(NWord, NState, NPos, 0),!.
         
